348e8129c71394e67e7e2ec7b86f8bb64b6e3aaa
Noah Thomas <noahthomas950@gmail.com>
2024-12-24 14:32:01 -0800

    Updated p7 for part two, completed part one of p8

--- p7.py
+++ p7.py
@@ -7,20 +7,24 @@ def _main():
     with open(FNAME, 'r') as fp:
         lines = fp.readlines()
     # solve
-    result = calibrate(lines)
+    result, result_2 = calibrate(lines)
     # Output result
     print(f'Part 1: Total calibration result: {result}')
+    print(f'Part 2: Total calibration result 2: {result_2}')
+
 
 
 def calibrate(lines):
-    cal_result = 0
+    cal_result, cal_result_2 = 0, 0
     for i, line in enumerate(lines):
         test_val = int(line.split(':')[0])
         nums = [int(x) for x in line.split(':')[1].strip().split(' ')]
         line_result = analyze_line(test_val, nums)
+        line_result_2 = analyze_line_2(test_val, nums)
         cal_result += line_result
-        print(f'{i+1:03d}/850 - [{line.strip()}] - {line_result}')
-    return cal_result
+        cal_result_2 += line_result_2
+        print(f'{i+1:03d}/850 - [{line.strip()}] - {line_result}, {line_result_2}')
+    return cal_result, cal_result_2
 
 
 def analyze_line(test_val, nums):
@@ -30,21 +34,53 @@ def analyze_line(test_val, nums):
     """
     total_operators = len(nums) - 1
     operator_combos = pow(2, total_operators)
-    res = 0
     for i in range(operator_combos):
         operator_str = f'{i:>016b}'[::-1]
         res = nums[0]
         for j, num in enumerate(nums[1:]):
             c = operator_str[j]
-            if c == '1':
-                res *= num
-            else:
-                res += num
+            res = res * num if c == '1' else res + num
             if res > test_val:
                 break
         if res == test_val:
             return res
     return 0
+
+def analyze_line_2(test_val, nums):
+    """
+    Determine if operators + and * can be combined to acheive a truthy equation
+    return test_val if truthy, else 0
+    """
+    total_operators = len(nums) - 1
+    operator_combos = pow(3, total_operators)
+    for i in range(operator_combos):
+        operator_str = f'{int(_ternary(i)):>016d}'[::-1]
+        res = nums[0]
+        for j, num in enumerate(nums[1:]):
+            c = operator_str[j]
+            if c == '0':
+                res += num
+            elif c == '1':
+                res *= num
+            else:
+                res = int(f'{res}{num}')
+            if res > test_val:
+                break
+        # print(operator_str, res)
+        if res == test_val:
+            return res
+    return 0
+
+
+def _ternary(n):
+    # convert int to ternary string
+    if n == 0:
+        return '0'
+    nums = []
+    while n:
+        n, r = divmod(n, 3)
+        nums.append(str(r))
+    return ''.join(reversed(nums))
 
 
 if __name__ == '__main__':
---
+++ p8.py
@@ -0,0 +1,97 @@
+#!/Users/noahthomas/Code/advent/venv/bin/python
+import numpy as np
+
+FNAME = 'input8'
+HEIGHT, WIDTH = 50, 50
+# FNAME = 'input8-2'
+# HEIGHT, WIDTH = 12, 12
+
+def _main():
+    # Read
+    with open(FNAME, 'r') as fp:
+        lines = fp.readlines()
+    
+    citymap = create_citymap(lines)
+    # solve
+    antinodes = set(find_antinodes(citymap))
+
+    # Output result
+    print(f'Part 1: Total unique antinodes: {len(antinodes)}')
+
+
+def create_citymap(lines):
+    """
+    Create citymap with the type, and location of every antenna
+    """
+    citymap = {}
+    for r, line in enumerate(lines):
+        for c, l in enumerate(line.strip()):
+            if l != '.':
+                if l not in citymap:
+                    citymap[l] = []
+                citymap[l].append((r, c))
+    return citymap
+
+
+def find_antinodes(citymap):
+    """
+    For every pair on the map, calculate their antinodes
+    """
+    antinodes = []
+    for k in citymap.keys():
+        antenna_coords = citymap[k]
+        antenna_pairs = get_pairs(antenna_coords)
+        for pair in antenna_pairs:
+            antinodes.extend(get_antinodes(pair))
+    return antinodes
+
+
+def get_pairs(coords):
+    return [(a, b) for idx, a in enumerate(coords) for b in coords[idx + 1:]]
+
+
+def get_antinodes(pair):
+    a, b = pair
+    diff = (b[0] - a[0], b[1] - a[1])
+    coords = [(a[0] - diff[0], a[1] - diff[1]), (b[0] + diff[0], b[1] + diff[1])]
+    antinodes = []
+    for coord in coords:
+        if in_map(coord):
+            antinodes.append(coord)
+    return antinodes
+
+
+def get_antinodes_2(pair):
+    a, b = pair
+    # diff is diff in rows, diff in cols, so 0,0, 2,5 would be 2,5
+    diff = (b[0] - a[0], b[1] - a[1])
+    r, c = (a[0] - diff[0], a[1] - diff[1])
+    antinodes = []
+    while in_map((r, c)):
+        antinodes.append((r, c))
+        r -= diff[0]
+        c -= diff[1]
+    r,c = (b[0] + diff[0], b[1] + diff[1])
+    while in_map((r, c)):
+        antinodes.append((r, c))
+        r += diff[0]
+        c += diff[1]
+    return antinodes
+
+    # 3,4, and 5,5
+    # diff is 2,1
+    # a = 1, 3
+    # b = 7,6
+
+    # 5,5 and 3,4
+    # diff is -2,-1
+    # b = 7,6
+    # a = 1, 3
+
+
+def in_map(node):
+    return node[0] >= 0 and node[0] < WIDTH and node[1] >= 0 and node[1] < HEIGHT
+
+
+if __name__ == '__main__':
+    _main()
