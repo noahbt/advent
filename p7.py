92ad3595dd00b49c455c08080def1bba5aeffd0b
Noah Thomas <noahthomas950@gmail.com>
2024-12-23 16:58:52 -0800

    Part 1 solved, ready for part 2

--- p7.py
+++ p7.py
@@ -1,5 +1,4 @@
 #!/Users/noahthomas/Code/advent/venv/bin/python
-import numpy as np
 
 FNAME = 'input7'
 
@@ -7,7 +6,7 @@ def _main():
     # Read
     with open(FNAME, 'r') as fp:
         lines = fp.readlines()
-    
+    # solve
     result = calibrate(lines)
     # Output result
     print(f'Part 1: Total calibration result: {result}')
@@ -16,23 +15,23 @@ def _main():
 def calibrate(lines):
     cal_result = 0
     for i, line in enumerate(lines):
-        print(f'{i+1:03d}/850 - {line}')
         test_val = int(line.split(':')[0])
         nums = [int(x) for x in line.split(':')[1].strip().split(' ')]
         line_result = analyze_line(test_val, nums)
         cal_result += line_result
+        print(f'{i+1:03d}/850 - [{line.strip()}] - {line_result}')
     return cal_result
 
 
 def analyze_line(test_val, nums):
-    # determine if operators + and * can be combined to acheive a truthy equation
-    # return test_val if truthy, else 0
+    """
+    Determine if operators + and * can be combined to acheive a truthy equation
+    return test_val if truthy, else 0
+    """
     total_operators = len(nums) - 1
-    # if you have 5 operators, you can do 00000 to 11111
     operator_combos = pow(2, total_operators)
     res = 0
     for i in range(operator_combos):
-        # 16 chose arbirtrarily, and reversed
         operator_str = f'{i:>016b}'[::-1]
         res = nums[0]
         for j, num in enumerate(nums[1:]):
@@ -41,7 +40,7 @@ def analyze_line(test_val, nums):
                 res *= num
             else:
                 res += num
-            if res >= test_val:
+            if res > test_val:
                 break
         if res == test_val:
             return res
