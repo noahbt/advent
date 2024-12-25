a0004046fcc4e1e8af77b46950b20bb7f15ada52
Noah Thomas <noahthomas950@gmail.com>
2024-12-25 10:55:57 -0800

    Got wrong answer because was using chars for ints

--- p9.py
+++ p9.py
@@ -1,7 +1,8 @@
 #!/Users/noahthomas/Code/advent/venv/bin/python
 import numpy as np
 
-FNAME = 'input8'
+# FNAME = 'input9'
+FNAME = 'input9-2'
 
 def _main():
     # Read
@@ -9,16 +10,30 @@ def _main():
         diskmap = fp.read()
     
     filemap = create_filemap(diskmap)
+    print(f'Filemap: {filemap}')
 
-    compacted = compact_filemap(filemap)
+    # compacted = compact_filemap(filemap)
+    # print(f'Compacted: {compacted}')
 
-    checksum = compute_checksum(compacted)
+    # checksum = compute_checksum(compacted)
 
     # Output result
-    print(f'Part 1: Computed checksum: {checksum}')
+    # print(f'Part 1: Computed checksum: {checksum}')
 
 
 def create_filemap(diskmap):
+    filemap = []
+    id = 0
+    for i, c in enumerate(diskmap):
+        if i % 2 == 0:
+            filemap.extend([id] * int(c))
+            id += 1
+        else:
+            filemap.extend([-1] * int(c))
+    return filemap
+
+
+def create_filemap_old(diskmap):
     """
     Convert from 12345 to 0..111....22222
     """
@@ -42,7 +57,7 @@ def compact_filemap(filemap):
     i = 0
     j = len(filemap) - 1
     result = ''
-    while i < j:
+    while i <= j:
         if filemap[i] != '.':
             result += filemap[i]
             i += 1
@@ -51,6 +66,9 @@ def compact_filemap(filemap):
                 result += filemap[j]
                 i += 1
             j -= 1
+    while i < len(filemap):
+        result += '.'
+        i += 1
     return result
     
 
@@ -61,10 +79,13 @@ def compute_checksum(compacted):
     """
     checksum = 0
     for i, c in enumerate(compacted):
-        checksum += i * int(c)
+        if c != '.':
+            # print(f'{i}*{int(c)}')
+            checksum += i * int(c)
     return checksum
-
 
 
 if __name__ == '__main__':
     _main()
+
+# 89312744865 was not right
