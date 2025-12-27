d4f7b660861a4bcd5149fc2a694ed56f4369116c
Noah Thomas <noahthomas950@gmail.com>
2025-12-27 08:26:44 -0800

    Completed 6

--- 2025/p6.py
+++ 2025/p6.py
@@ -2,8 +2,8 @@
 
 TEST_INPUT = [
     "123 328  51 64 ",
-    "45 64  387 23 ",
-    "6 98  215 314",
+    " 45 64  387 23 ",
+    "  6 98  215 314",
     "*   +   *   +  "
 ]
 
@@ -11,7 +11,7 @@ def main():
     with open('input6.txt', 'r') as f:
         data = f.readlines()
     # data = TEST_INPUT
-    total = calculate_expression_sum(data)
+    total = calculate_expression_sum2(data)
     print(f"Total sum of expressions: {total}")
 
 def calculate_expression_sum(data):
@@ -35,6 +35,47 @@ def calculate_expression_sum(data):
                 print(f"Unknown operator: {operator}")
     total_sum = sum(result_arr)
     return total_sum
+
+def calculate_expression_sum2(data):
+    # same thing as last time, except the numbers are top down instead of left to right
+    line_length = len(data[0].strip())
+    operators_line = data[-1].strip().split()
+    result_arr = []
+
+    op_i = 0
+    col = 0
+    # read columns until you get all spaces, then perform the operation on the resulting numbers
+    # each column is a number
+    numbers = []
+    while col < line_length:
+        num_str = ''
+        for row in range(len(data) - 1):
+            char = data[row][col]
+            if char != ' ':
+                num_str += char
+        if num_str == '':
+            # perform operation on numbers and add this to the the result array
+            result = calculate_result(numbers, operators_line[op_i])
+            result_arr.append(result)
+            op_i += 1
+            numbers = []
+        else:
+            numbers.append(int(num_str))
+        col += 1
+    result = calculate_result(numbers, operators_line[op_i])
+    result_arr.append(result)
+    total_sum = sum(result_arr)
+    assert len(result_arr) == len(operators_line)
+    return total_sum
+
+def calculate_result(numbers, operator):
+    if operator == '*':
+        prod = 1
+        for n in numbers:
+            prod *= n
+        return prod
+    elif operator == '+':
+        return sum(numbers)
 
 if __name__ == "__main__":
     main()
